611d61e7a615f72c57de502d4f5a24ff3aec492e
Yurii Rochniak <yrochnyak@gmail.com>
2018-01-25 10:59:45 +0200

    test concourse ci

---
+++ concourse_run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: concourse_run.sh
+# 
+#         USAGE: ./concourse_run.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 01/25/2018 10:56
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+bundle exec rake default
+gem build sensu-plugins-druid.gemspec
+gem install sensu-plugins-druid-*.gem
+
